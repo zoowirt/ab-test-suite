33ca19efd878bb7ffb07f2797cdb1d9aefb25bbe
Martin <martin.meyries@dataqube.de>
2020-02-05 20:58:21 +0100

    port change

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app.secret_key = "my_secret_key"
 app.register_blueprint(routes)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, use_reloader=False)
+    app.run(threaded=True, port=5000)
